1372611cd20e2cf3c9f2bb42e4e0d93090a16bc9
Rhys Evans <rhys.evans@ft.com>
2014-05-14 15:31:33 +0100

    fixed inline head script copying

--- tasks/inline-head-script.js
+++ tasks/inline-head-script.js
@@ -5,17 +5,17 @@ module.exports = function(grunt) {
     var ftConfig = require('../grunt-config')().ft;
 
     grunt.registerTask('inline-head-script', 'Removes whitespace from line-endings of javascript files', function (mode) {
-        var pathToInlineHeadScript = '<%= ft.srcPath %>' + (ftConfig.isModular ? '' : 'js/') + 'head/inlineScript.mustache';
+        var pathToInlineHeadScript = ftConfig.srcPath + (ftConfig.isModular ? '' : 'js/') + 'head/inlineScript.mustache';
         var script = grunt.file.read(pathToInlineHeadScript);
-        var path = require('path')
+        var path = require('path');
 
         if (mode === 'dev') {
             script = script.replace(/\s*\n\s*/g, '');
         }
 
         ftConfig.inlineHeadScriptDestinations.forEach(function (dest) {
-            grunt.file.write(dest, script);
-            grunt.log.writeln('Copied inline head script to' + path.join(process.cwd(), dest));
+            grunt.file.write(path.join(process.cwd(), dest + 'inlineScript.mustache'), script);
+            grunt.log.writeln('Copied inline head script to' + dest);
         });
     });
 
