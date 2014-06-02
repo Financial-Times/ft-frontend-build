5fe781bc97a08a7182a6be636c5c1658eee2d490
Rhys Evans <rhys.evans@ft.com>
2014-06-02 16:20:17 +0100

    tidy up

--- test/ft-frontend-build_spec.js
+++ test/ft-frontend-build_spec.js
@@ -1,9 +1,14 @@
-require('./test_init');
-
+var wrench = require('wrench');
 var test = require('./single_test');
 
-var path = require('path');
-var j2f = require('jsonToFs');
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 30000000;
+
+// copy src into node_modules/ft-frontend-build
+wrench.copyDirSyncRecursive('./', 'test/dummy-project/node_modules/ft-frontend-build', {
+    forceDelete: true, // Whether to overwrite existing directory or not
+    preserveFiles: false, // If we're overwriting something and the file already exists, keep the existing
+    exclude: /node_modules/ // An exclude filter (either a regexp or a function)
+});
 
 describe('ft-frontend-build', function () {
     var cwd;
